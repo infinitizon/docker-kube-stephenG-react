5bf156166b58676e8355dda768bb6636ec9bbbd1
Abimbola Hassan <AHassan@chapelhilldenham.com>
2023-04-15 22:18:58 +0100

    Slight changes to trigger travis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. This is importantest
+          Edit <code>src/App.js</code> and save to reload. This is importanta
         </p>
         <a
           className="App-link"
